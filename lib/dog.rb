aeb5b26530219ee83ffef0ec26fc90ebe57d6880
James Lenahan <james.k.lenahan@gmail.com>
2020-01-20 15:54:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,12 @@
 class Dog
-  attr_reader :id, :name, :breed
-  
+  attr_accessor :id, :name, :breed
+
   def initialize(attributes)
+    #id: nil, name:, breed:
     attributes.each {|key, value| self.send(("#{key}="), value)}
     self.id ||= nil
   end
+
   def self.create_table
     sql = <<-SQL
       CREATE TABLE IF NOT EXISTS dogs (
@@ -16,7 +18,34 @@ class Dog
 
       DB[:conn].execute(sql)
   end
-    def self.find_by_id(id)
+
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE dogs
+      SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO dogs (name, breed) VALUES (?, ?)
+      SQL
+
+    DB[:conn].execute(sql, self.name, self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+
+    self
+  end
+
+  def self.create(hash_of_attributes)
+      dog = self.new(hash_of_attributes)
+      dog.save
+
+      dog
+  end
+
+  def self.find_by_id(id)
     sql = <<-SQL
       SELECT * FROM dogs WHERE id = ?
     SQL
@@ -69,5 +98,5 @@ class Dog
 
       DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
-end
 
+end
