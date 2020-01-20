c8871b0f34c3445a57446803fdf54cb471c1a428
James Lenahan <james.k.lenahan@gmail.com>
2020-01-20 15:34:15 +0000

    Automatically backed up by Learn

--- spec/dog_spec.rb
+++ spec/dog_spec.rb
@@ -46,11 +46,12 @@ describe "Dog" do
     expect(DB[:conn].execute(table_check_sql)[0]).to eq(['dogs'])
   end
 
-  describe ".drop_table" do
-    it 'drops the dogs table from the database' do
-      Dog.drop_table
+  describe '::drop_table' do
+    it "drops the dogs table" do
+        Dog.drop_table
+ 
       table_check_sql = "SELECT tbl_name FROM sqlite_master WHERE type='table' AND tbl_name='dogs';"
-      expect(DB[:conn].execute(table_check_sql)[0]).to eq(nil)
+      expect(DB[:conn].execute(table_check_sql)[0]).to be_nil
     end
   end
 
